f9f475b7a69a71cfd5bbf00ab38f0bd0ae665ad9
huy <huyvh25@gmail.com>
2025-09-25 15:09:13 +0700

    finished toto-app with js

--- 1.html-css-js/3.project/1.todo-list-app/script.js
+++ 1.html-css-js/3.project/1.todo-list-app/script.js
@@ -1,10 +1,13 @@
 const tasks = [];
 let btnAdd = document.querySelector(".btn-add");
 let listHtml = document.querySelector(".list-tasks");
+let listStatus = document.querySelector(".list-status");
 let task = {
     name: '',
-    status: false
+    status: true
 };
+let indexGlobal;
+
 // tạo sự kiến lắng nghe cho input 
 document.querySelector("#input-text").addEventListener('input', function () {
     task.name = this.value;
@@ -13,18 +16,17 @@ document.querySelector("#input-text").addEventListener('input', function () {
 // sự kiện add task vào list
 btnAdd.addEventListener('click', function () {
     tasks.push({ name: task.name, status: false })
-    console.log("tasks : ", tasks);
     renderHtmlList(tasks)
 })
-
-const renderHtmlList = (tasks) => {
+// render list 
+const renderHtmlList = (tasks, globalStatus = 'all') => {
     listHtml.innerText = '';
     tasks.map((item, index) => {
-        renderHtmlItem(item.name, index);
+        renderHtmlItem(item.name, item.status, index, globalStatus);
     })
 }
-const renderHtmlItem = (content, index) => {
-    console.log('content : ', content, ' index : ', index);
+// render html 
+const renderHtmlItem = (content, status, index, globalStatus) => {
     // tạo li 
     let li = document.createElement("li");
     // tạo thẻ a và btn - delete 
@@ -32,19 +34,87 @@ const renderHtmlItem = (content, index) => {
     let btn = document.createElement("button");
     let input = document.createElement("input");
     input.type = 'checkbox';
-
     a.innerText = content;
-    btn.innerText = 'xóa';
-
+    btn.innerHTML = '<i class="fa-solid fa-trash"></i>';
+    btn.classList = 'btn-remove'
     btn.addEventListener('click', function () {
-        console.log('xóa vị trí : ', index);
+        deleteItemByIndex(index)
     })
+    if (status) {
+        input.checked = true;
+        li.classList.add('finished');
+    } else {
+        li.classList.remove('finished');
+        input.checked = false;
+    }
     input.addEventListener('change', function () {
-        console.log('checked vị  trí : ', index);
+        setNewStatus(index);
     })
-    li.appendChild(a)
+    if (globalStatus === 'finished') {
+        if (status !== true) {
+            li.style.display = "none";
+        }
+    }
+    if (globalStatus === 'unfinished') {
+        if (status !== false) {
+            li.style.display = "none";
+        }
+    }
     li.appendChild(input);
+    li.appendChild(a)
     li.appendChild(btn)
-
     listHtml.appendChild(li);
 }
+// delete task
+const deleteItemByIndex = (index) => {
+    tasks.splice(index, 1);
+    renderHtmlList(tasks)
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+// đổi trạng thái task
+const setNewStatus = (index) => {
+    let toggle = tasks[index].status;
+    let newStatus = !toggle;
+    tasks[index].status = newStatus;
+    renderHtmlList(tasks);
+}
+
+// render status
+const renderStatus = () => {
+    let btn1 = document.createElement("button")
+    let btn2 = document.createElement("button")
+    let btn3 = document.createElement("button")
+    // gán nội dung 
+    btn1.innerText = 'all'
+    btn2.innerText = 'finished'
+    btn3.innerText = 'unfinished'
+    // gán sự kiện
+    btn1.addEventListener('click', function () {
+        // sử lý lọc
+        btn1.style.backgroundColor = 'green'
+        btn2.style.backgroundColor = 'red'
+        btn3.style.backgroundColor = 'red'
+        renderHtmlList(tasks, this.innerText.trim())
+    })
+
+    btn2.addEventListener('click', function () {
+        // sử lý học finished  <=> true
+        btn2.style.backgroundColor = 'green'
+        btn1.style.backgroundColor = 'red'
+        btn3.style.backgroundColor = 'red'
+        renderHtmlList(tasks, this.innerText.trim())
+    })
+    btn3.addEventListener('click', function () {
+        // sử lý học unfinished  <=> false
+        btn1.style.backgroundColor = 'red'
+        btn2.style.backgroundColor = 'red'
+        btn3.style.backgroundColor = 'green'
+        renderHtmlList(tasks, this.innerText.trim())
+    })
+    listStatus.appendChild(btn1)
+    listStatus.appendChild(btn2)
+    listStatus.appendChild(btn3)
+}
+
+
+renderStatus();
